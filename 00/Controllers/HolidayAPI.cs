b2ac2fac187921916b7f37c9fdf29ff62da8a6ea
IntsFedotovskis <ints.fedotovskis@gmail.com>
2022-06-19 16:22:43 +0300

    init

---
+++ 00/Controllers/HolidayAPI.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _00.Controllers
+{
+    
+        
+    
+}
